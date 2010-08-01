623b75424d93dba5c26b401a2d441bdf5ea1fd88
WrinklyNinja <wrinklyninja1@gmail.com>
2010-08-01 13:59:01 +0000

    Another branch, this time for trying out implementing custom user sorting rules for mods not included in the masterlist.
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@1241 bfee223e-1fc4-11de-b650-e715bd6d7cf1

---
+++ code/boss-common/branches/wrinklyninja-user-rules/Support/Helpers.cpp
@@ -0,0 +1,104 @@
+/*	Better Oblivion Sorting Software
+	2.0 Beta
+	Quick and Dirty Load Order Utility
+	(Making C++ look like the scripting language it isn't.)
+
+    	Copyright (C) 2009-2010  Random/Random007/jpearce & the BOSS development team
+    	http://creativecommons.org/licenses/by-nc-nd/3.0/
+
+	 $Id: Helpers.cpp 1200 2010-07-29 22:51:09Z leandor@gmail.com $
+	$URL: https://better-oblivion-sorting-software.googlecode.com/svn/BOSS%20source%20code/Support/Helpers.cpp $
+*/
+
+#include <strstream>
+
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <Support/Types.h>
+#include <Support/Helpers.h>
+#include <Support/VersionRegex.h>
+
+namespace boss {
+
+using namespace std;
+using namespace boost;
+
+// Reads a text line from the input stream
+bool ReadLine(istream& is, string& s)
+{
+	s.erase();
+	while(is)
+	{
+		// Get next char in input buffer
+		char c = static_cast<char>(is.get());
+
+		// Check for termination conditions
+		if (c <= 0 || c == '\r' || c == '\n') {
+			if (c > 0){
+				// If EOLN then consume the second char in the '\r\n' pair
+				c = static_cast<char>(is.peek());
+				if (c == '\n' || c == '\r') {
+					is.get();
+				}
+			}
+
+			return true;
+		}
+
+		// While termination condition not found -> append chars to result string
+		s.append(1, c);
+	}
+
+	return !s.empty();
+}
+
+// Reads a string until the terminator char is found or the complete buffer is consumed.
+string ReadString(char*& bufptr, ushort size){
+	string data;
+	
+	data.reserve(size + 1);
+	while (char c = *bufptr++) {
+		data.append(1, c);
+	}
+
+	return data;
+}
+
+// Tries to parse the textual string to find a suitable version indication.
+string ParseVersion(const string& text){
+
+	smatch what;
+
+	regex* re;
+	for(int i = 0; re = version_checks[i]; i++) {
+
+		string data;
+		istrstream iss(text.c_str(), text.length());
+		while (ReadLine(iss, data)) {
+
+			if (data.empty()){
+				continue;
+			}
+
+			if (regex_match(data, what, *re)){
+				
+				if (what.empty()){
+					continue;
+				}
+
+				ssub_match match = what[2];
+		
+				if (!match.matched) {
+					continue;
+				}
+
+				return trim_copy(string(match.first, match.second));
+			}
+		}
+	}
+
+	return string();
+}
+
+};
---
+++ code/boss-common/branches/wrinklyninja-user-rules/Support/ModFormat.h
@@ -0,0 +1,46 @@
+/*	Better Oblivion Sorting Software
+	2.0 Beta
+	Quick and Dirty Load Order Utility
+	(Making C++ look like the scripting language it isn't.)
+
+    	Copyright (C) 2009-2010  Random/Random007/jpearce & the BOSS development team
+    	http://creativecommons.org/licenses/by-nc-nd/3.0/
+
+	 $Id: ModFormat.h 1200 2010-07-29 22:51:09Z leandor@gmail.com $
+	$URL: https://better-oblivion-sorting-software.googlecode.com/svn/BOSS%20source%20code/Support/ModFormat.h $
+*/
+
+#ifndef __SUPPORT_MODFORMAT__HPP__
+#define __SUPPORT_MODFORMAT__HPP__
+
+#include <cstring>
+
+#include <Support/Types.h>
+
+namespace boss {
+
+	using namespace std;
+
+	// Structure for grouping the information gathered from each mod's header.
+	struct ModHeader {
+		string		Name;
+		string		Description;
+		string		Author;
+		string		Version;
+		bool		IsMaster;
+	};
+
+
+	struct Record {
+		static const ulong TES4	=	'4SET';
+		static const ulong HEDR	=	'RDEH';
+		static const ulong OFST	=	'TSFO';
+		static const ulong DELE	=	'ELED';
+		static const ulong CNAM	=	'MANC';
+		static const ulong SNAM	=	'MANS';
+	};
+
+	ModHeader ReadHeader(string filename);
+};
+
+#endif __SUPPORT_MODFORMAT__HPP__
