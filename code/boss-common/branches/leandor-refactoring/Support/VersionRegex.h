e413e190fbad8c27ca3018a4e4794a6209cf6431
leandor <leandor@gmail.com>
2010-08-01 04:45:11 +0000

    CHANGE: Trivial format update to the file headers (remove version number, added keywords)
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@1233 bfee223e-1fc4-11de-b650-e715bd6d7cf1

--- code/boss-common/branches/leandor-refactoring/Support/VersionRegex.h
+++ code/boss-common/branches/leandor-refactoring/Support/VersionRegex.h
@@ -1,13 +1,12 @@
 /*	Better Oblivion Sorting Software
-	2.0 Beta
+	
 	Quick and Dirty Load Order Utility
 	(Making C++ look like the scripting language it isn't.)
 
-    	Copyright (C) 2009-2010  Random/Random007/jpearce & the BOSS development team
-    	http://creativecommons.org/licenses/by-nc-nd/3.0/
+    Copyright (C) 2009-2010  Random/Random007/jpearce & the BOSS development team
+    http://creativecommons.org/licenses/by-nc-nd/3.0/
 
-	 $Id: VersionRegex.h 1200 2010-07-29 22:51:09Z leandor@gmail.com $
-	$URL: https://better-oblivion-sorting-software.googlecode.com/svn/BOSS%20source%20code/Support/VersionRegex.h $
+	$Revision$, $Date$
 */
 
 #ifndef __SUPPORT_VERSIONREGEX__HPP__
