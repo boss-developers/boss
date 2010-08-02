26bfa0a60581a7242ce6a39f24f6d172f3138224
leandor <leandor@gmail.com>
2010-08-02 01:48:12 +0000

    CHANGE: Changed the RegexTesting program to use the input file as source of data instead of scanning the mods in the Data folder. This way, the extracted version string can be checked against the expected version which is found in the file (edited by hand to have the correct one). The program now outputs the mods that fails the check, i.e. the ones for which version extracted != expected.
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@1253 bfee223e-1fc4-11de-b650-e715bd6d7cf1

--- code/boss-common/branches/leandor-refactoring/Testing/RegexTesting/RegexTesting.cpp
+++ code/boss-common/branches/leandor-refactoring/Testing/RegexTesting/RegexTesting.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <boost/format.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/convenience.hpp>
@@ -17,48 +18,71 @@ using namespace boost::filesystem;
 using namespace boost::algorithm;
 using namespace boss;
 
-
-string Trim(string& text) {
-	
-	return text;
-}
+boost::format InvalidInputError = boost::format("Invalid input file format. Expected: '%1%' but found '%2%'");
 
 int main(int argc, char* argv[])
 {
-	directory_iterator end;
-	path root = current_path();
-
-	path outname = root / "testing.txt";
-	remove(outname);
-
-	ofstream out(outname.filename());
-	out << "Running on: " << root << endl << endl;
-
-	for (directory_iterator it(root); it != end; it++) {
-
-		if (is_directory(it->status())) {
-			continue;
-		}
-
-		path item = it->path();
-		
-		string ext = extension(*it);
-		to_lower(ext);
-
-		if (ext == ".esp" || ext == ".esm") {
-
-			ModHeader header = ReadHeader(item.filename());
-
-			out << " * " << item.filename() << endl;
-
-			if (!header.Version.empty()) {
-				out << "  * VERSION: [" << trim_copy(header.Version) << "]" << endl;
-			}
-
-			out << "  * DESCR: [" << replace_all_copy(header.Description, "\r\n", "|") << "]" << endl << endl;
-		}
-	}
+	try
+	{
+		path root = current_path();
 	
-	return 0;
+		path inname = root / "VersionCheck.txt";
+	
+		ifstream in(inname.filename());
+	
+		cout << "Reading: " << inname.filename() << endl << endl;
+	
+		string line;
+		while (in) {
+	
+			while (in) {
+				if (! ReadLine(in, line)) {
+					return 0;
+				}
+
+				trim(line);
+
+				if (! line.empty()) 
+					break;
+			}
+	
+			if (!starts_with(line, "FILENAME:")) throw exception((InvalidInputError % "FILENAME:" % line).str().c_str());
+			replace_first(line, "FILENAME:", "");
+			trim(line);
+			string modfile = line;
+	
+			if (! ReadLine(in, line)) {
+				throw exception("No more data to read.");
+			}
+	
+			trim(line);
+			if (!starts_with(line, "DESCRIP:")) throw exception((InvalidInputError % "DESCRIP:" % line).str().c_str());
+			replace_first(line, "DESCRIP:", "");
+			replace_all(line, "|", "\n");
+			string descr = line;
+	
+			if (! ReadLine(in, line)) {
+				throw exception("No more data to read.");
+			}
+	
+			trim(line);
+			if (!starts_with(line, "VERSION:")) throw exception((InvalidInputError % "VERSION:" % line).str().c_str());
+			replace_first(line, "VERSION:", "");
+			string version = line;		
+	
+			string text = ParseVersion(descr);
+	
+			if (!equals(text, version)) {
+				cout << (boost::format("%1%: Failed.\n - Expected: [%2%]\n - Extracted [%3%]") % modfile % version % text).str() << endl << endl;
+			}
+		}
+		
+		return 0;
+	}
+	catch (exception& e)
+	{
+		cerr << "Error: " << e.what() << endl;
+		return 1;
+	}
 }
 
