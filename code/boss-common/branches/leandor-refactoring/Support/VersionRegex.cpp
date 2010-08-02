c3ae34407458ed483829c301267f6f5cc49f316b
leandor <leandor@gmail.com>
2010-08-02 03:29:04 +0000

    CHANGE: Updated a bit the regular expressions: changed them to only have one matching group, and splited several into successive options to avoid causing incorrect early matches. Now they catch all but 16 of the 197 mods checked. I'll add more mods to the list as I have some extra time.
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@1259 bfee223e-1fc4-11de-b650-e715bd6d7cf1

--- code/boss-common/branches/leandor-refactoring/Support/VersionRegex.cpp
+++ code/boss-common/branches/leandor-refactoring/Support/VersionRegex.cpp
@@ -9,10 +9,12 @@
 	$Revision$, $Date$
 */
 
+#include <string>
 #include <boost/regex.hpp>
 
 namespace boss {
 
+	using namespace std;
 	using namespace boost;
 
 	/// REGEX expression definition
@@ -20,40 +22,39 @@ namespace boss {
 	///    1. The marker string "version", "ver", "rev", "v" or "r"
 	///    2. The version string itself.
 
-	// Matches "Version: 12.33", "Ver 1.23", "Rev 1.23"
-	const char* re1 = 
-			"(version[:\\. ]*|ver[:\\. ]*|rev[:\\. ]*) *"
-			"([-0-9a-zA-Z\\._ ]+\\+?)"
-			"(.*)"
-			;
+	const char* regex1 = 
+		"(?:\s*)"
+		"(?:version(?:[:.\\- \\t]*)|revision(?:[:.\\- \\t]*))[ ]*"
+		"([-0-9a-zA-Z._ ]+\\+?)$"
+		;
 
-	// Matches "R12.3" and "V1.23"
-	const char* re2 = 
-			"(\\(?r[:\\. ]*\\)?|\\(?v[:\\. ]*\\)?) *"
-			"([-0-9a-zA-Z\\._ ]+\\+?)"
-			"(.*)"
-			;
+	const char* regex2 = 
+		"(?:\s*)"
+		"(?:ver(?:[:.\\- \\t]*)|rev(?:[:.\\- \\t]*))[ ]*"
+		"([-0-9a-zA-Z._ ]+\\+?)$"
+		;
 
 	// Matches "Updated: <date>" for the Bashed patch
-	const char* re3 = 
-			"(Updated:) *"
-			"([-0-9a-zA-Z/\\._ ]+\\+?)"
-			"(.*)"
-			;
+	const char* regex3 = 
+		"(?:Updated:)\\s*"
+		"([-0-9aAmMpP/ :]+)$"
+		;
 
 	// Matches isolated versions as last resort
-	const char* re4 = 
-			"([^0-9]+) *"
-			"([0-9][-0-9a-zA-Z\\._ ]*\\+?)"
-			"(.*)"
-			;
+	const char* regex4 = 
+		"(?:(?:v|r)(?:\s?)(?:[-.:])?(?:\s*))"
+		"((?:(?:alpha)?|(?:beta)?|(?:mark)?)\s*[0-9][-0-9a-zA-Z._]*\\+?)"
+		;
 
 	/// Array used to try each of the expressions defined above using 
 	/// an iteration for each of them.
 	regex* version_checks[] = {
-			new regex(re1, regex::icase | regex::extended),
-			new regex(re2, regex::icase | regex::extended),
-			new regex(re3, regex::icase | regex::extended),
+			new regex(regex1, regex::icase),
+			new regex(regex2, regex::icase),
+			new regex(regex3, regex::icase),
+			new regex(regex4, regex::icase),
+			//new regex(re2, regex::icase | regex::extended | regex::nosubs),
+			//new regex(re3, regex::icase | regex::extended | regex::nosubs),
 			0
 			};
 
