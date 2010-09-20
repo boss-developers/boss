530a373b800e40214ae73787bbe16af9d85cb32e
leandor <leandor@gmail.com>
2010-09-20 22:45:49 +0000

    BRANCH: Created new private branch for trying to fix a detected issue present in current trunk
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@1521 bfee223e-1fc4-11de-b650-e715bd6d7cf1

---
+++ code/boss-common/branches/leandor-fixes/Support/Helpers.h
@@ -0,0 +1,87 @@
+/*	Better Oblivion Sorting Software
+	
+	Quick and Dirty Load Order Utility
+	(Making C++ look like the scripting language it isn't.)
+
+    Copyright (C) 2009-2010  Random/Random007/jpearce & the BOSS development team
+    http://creativecommons.org/licenses/by-nc-nd/3.0/
+
+	$Revision$, $Date$
+*/
+
+#ifndef __SUPPORT_HELPERS__HPP__
+#define __SUPPORT_HELPERS__HPP__
+
+#include <cstring>
+#include <iostream>
+
+#ifdef WIN32
+#include <Support/Types.h>
+#else
+#include "../Support/Types.h"
+#endif
+
+namespace boss {
+	using namespace std;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Helper functions
+	//////////////////////////////////////////////////////////////////////////
+
+
+	//
+	// string ParseVersion(string&):
+	//	- Tries to extract the version string value from the given text,
+	//	using the above defined regexes to do the dirty work.
+	//
+	string ParseVersion(const string& text);
+
+	//
+	// bool ReadLine(InputStream, OutputString&):
+	//	- Reads a text line from the received input stream.
+	//
+	bool ReadLine(istream& is, string& s);
+
+	/// bool GetLine(istream& is, string& s)
+	///  - Reads a text line skipping all the empty lines along the way
+	bool GetLine(istream& is, string& s);
+
+	/// bool GetLine(istream& is, string& s)
+	///  - Reads a text line skipping all the empty lines along the way
+	string GetLine(istream& is);
+
+	//
+	// string ReadString(pointer&, maxsize):
+	//	- Reads a consecutive array of charactes up to maxsize length and 
+	//	returns them as a new string.
+	//
+	string ReadString(char*& bufptr, ushort size = 512);
+
+	//
+	// T Peek<T>(pointer&):
+	//	- Peeks into the received buffer and returns the value pointed 
+	//	converting it to the type T.
+	//
+	template <typename T> 
+	T Peek(char* buffer) {
+		return *reinterpret_cast<T*>(buffer);
+	}
+
+	//
+	// T Read<T>(pointer&):
+	//	- Tries to extract a value of the specified type T from the 
+	//	received buffer, incrementing the pointer to point past the readen 
+	//	value.
+	//
+	template <typename T> 
+	inline T Read(char*& buffer) {
+		T value = Peek<T>(buffer);
+		buffer += sizeof(T);
+		return value;
+	}
+
+	//Changes uppercase to lowercase and removes trailing spaces to do what Windows filesystem does to filenames.	
+	string Tidy(string filename);
+};
+
+#endif
