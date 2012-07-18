17281fd1aee194140c87d4fb86a8d2040025897a
WrinklyNinja <wrinklyninja1@gmail.com>
2012-07-18 13:53:01 +0000

    - Updated translation batch files, so there's only one human-readable file generator that does the functionality of both the old ones, and gave them more obvious names. - Added a link to the regex flavour spec used by BOSS to the Masterlist Syntax doc. - Removed translation instructions from the i18n wiki page. - Added translation instructions to the readme.
    git-svn-id: http://better-oblivion-sorting-software.googlecode.com/svn@4818 bfee223e-1fc4-11de-b650-e715bd6d7cf1

--- code/trunk/translations/GenReadableTransFile.bat
+++
@@ -1,1 +0,0 @@
-cmd /k "xgettext --keyword=translate:1,1t --directory=..\boss-common\Common --directory=..\boss-gui\GUI --directory=. Error.h Error.cpp Game.cpp ..\boss-common\Output\Output.cpp ..\boss-cli\BOSS-CLI.cpp MainWindow.cpp SettingsWindow.cpp UserRuleEditor.cpp"
--- code/trunk/translations/GenReadableTransFileFromExisting.bat
+++
@@ -1,3 +0,0 @@
-cmd /k "msgunfmt --output-file=oldMessages.po messages.mo && ^
- xgettext --output=newMessages.po --keyword=translate:1,1t --directory=..\boss-common\Common --directory=..\boss-gui\GUI --directory=. Error.h Error.cpp Game.cpp ..\boss-common\Output\Output.cpp ..\boss-cli\BOSS-CLI.cpp MainWindow.cpp SettingsWindow.cpp UserRuleEditor.cpp && ^
- msgmerge --output-file=messages.po oldMessages.po newMessages.po"
