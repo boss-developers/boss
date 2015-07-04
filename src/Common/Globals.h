82a9680020938f05a52d1e64855680fd3abad8f9
deaths_soul (MCP) <NULL>
2015-07-04 10:40:51 -0400

    Deindenting the namespace in Common/Globals.h

--- src/Common/Globals.h
+++ src/Common/Globals.h
@@ -41,83 +41,85 @@
 #include "Common/Game.h"
 
 namespace boss {
-	using namespace std;
-	namespace fs = boost::filesystem;
 
-	// BOSS version number.
-	BOSS_COMMON extern const uint32_t BOSS_VERSION_MAJOR;
-	BOSS_COMMON extern const uint32_t BOSS_VERSION_MINOR;
-	BOSS_COMMON extern const uint32_t BOSS_VERSION_PATCH;
+using namespace std;
+namespace fs = boost::filesystem;
 
-	/*
-	 * MCP Note: Possibly change these to enums?
-	 * For all these notes, need to fully investigate the scope.
-	 */
-	// Supported game values.
-	BOSS_COMMON extern const uint32_t AUTODETECT;
-	BOSS_COMMON extern const uint32_t OBLIVION;
-	BOSS_COMMON extern const uint32_t NEHRIM;
-	BOSS_COMMON extern const uint32_t SKYRIM;
-	BOSS_COMMON extern const uint32_t FALLOUT3;
-	BOSS_COMMON extern const uint32_t FALLOUTNV;
+// BOSS version number.
+BOSS_COMMON extern const uint32_t BOSS_VERSION_MAJOR;
+BOSS_COMMON extern const uint32_t BOSS_VERSION_MINOR;
+BOSS_COMMON extern const uint32_t BOSS_VERSION_PATCH;
 
-	// BOSS Log formatting values.
-	BOSS_COMMON extern const uint32_t HTML;
-	BOSS_COMMON extern const uint32_t PLAINTEXT;
+/*
+ * MCP Note: Possibly change these to enums?
+ * For all these notes, need to fully investigate the scope.
+ */
+// Supported game values.
+BOSS_COMMON extern const uint32_t AUTODETECT;
+BOSS_COMMON extern const uint32_t OBLIVION;
+BOSS_COMMON extern const uint32_t NEHRIM;
+BOSS_COMMON extern const uint32_t SKYRIM;
+BOSS_COMMON extern const uint32_t FALLOUT3;
+BOSS_COMMON extern const uint32_t FALLOUTNV;
 
-	// Language values.
-	BOSS_COMMON extern const uint32_t ENGLISH;
-	BOSS_COMMON extern const uint32_t SPANISH;
-	BOSS_COMMON extern const uint32_t GERMAN;
-	BOSS_COMMON extern const uint32_t RUSSIAN;
-	BOSS_COMMON extern const uint32_t SIMPCHINESE;
+// BOSS Log formatting values.
+BOSS_COMMON extern const uint32_t HTML;
+BOSS_COMMON extern const uint32_t PLAINTEXT;
 
-
-	///////////////////////////////
-	// File/Folder Paths
-	///////////////////////////////
-
-	// Paths that are game-invariant.
-	BOSS_COMMON extern const fs::path boss_path;  // Path to the BOSS folder, relative to executable (ie. '.').
-	BOSS_COMMON extern const fs::path ini_path;
-	BOSS_COMMON extern const fs::path old_ini_path;
-	BOSS_COMMON extern const fs::path debug_log_path;
-	BOSS_COMMON extern const fs::path readme_path;
-	BOSS_COMMON extern const fs::path rules_readme_path;
-	BOSS_COMMON extern const fs::path masterlist_doc_path;
-	BOSS_COMMON extern const fs::path api_doc_path;
-	BOSS_COMMON extern const fs::path version_history_path;
-	BOSS_COMMON extern const fs::path licenses_path;
-	BOSS_COMMON extern const fs::path l10n_path;
+// Language values.
+BOSS_COMMON extern const uint32_t ENGLISH;
+BOSS_COMMON extern const uint32_t SPANISH;
+BOSS_COMMON extern const uint32_t GERMAN;
+BOSS_COMMON extern const uint32_t RUSSIAN;
+BOSS_COMMON extern const uint32_t SIMPCHINESE;
 
 
-	///////////////////////////////
-	// Ini Settings
-	///////////////////////////////
-	// These globals exist for ease-of-use, so that a Settings object doesn't need to be passed in infinity+1 functions.
+///////////////////////////////
+// File/Folder Paths
+///////////////////////////////
 
-	// General variables
-	BOSS_COMMON extern bool gl_use_user_rules_manager;   // Use the User Rules Editor or edit userlist.txt directly?
-	BOSS_COMMON extern bool gl_close_gui_after_sorting;  // Close the GUI after BOSS has finished running or not.
-	BOSS_COMMON extern uint32_t gl_language;             // What language to run BOSS in?
+// Paths that are game-invariant.
+BOSS_COMMON extern const fs::path boss_path;  // Path to the BOSS folder, relative to executable (ie. '.').
+BOSS_COMMON extern const fs::path ini_path;
+BOSS_COMMON extern const fs::path old_ini_path;
+BOSS_COMMON extern const fs::path debug_log_path;
+BOSS_COMMON extern const fs::path readme_path;
+BOSS_COMMON extern const fs::path rules_readme_path;
+BOSS_COMMON extern const fs::path masterlist_doc_path;
+BOSS_COMMON extern const fs::path api_doc_path;
+BOSS_COMMON extern const fs::path version_history_path;
+BOSS_COMMON extern const fs::path licenses_path;
+BOSS_COMMON extern const fs::path l10n_path;
 
-	// Command line variables
-	BOSS_COMMON extern uint32_t gl_log_format;       // What format the output should be in.  Uses the enums defined above.
-	BOSS_COMMON extern uint32_t gl_game;             // What game's mods are we sorting? Uses the enums defined above.
-	BOSS_COMMON extern uint32_t gl_last_game;        // What game was last run? Only affects GUI behaviour.
-	BOSS_COMMON extern uint32_t gl_revert;           // What level to revert to
-	BOSS_COMMON extern uint32_t gl_debug_verbosity;  // Log levels above INFO to output
-	BOSS_COMMON extern bool gl_update;               // Update the masterlist?
-	BOSS_COMMON extern bool gl_update_only;          // Only update the masterlist and don't sort currently.
-	BOSS_COMMON extern bool gl_silent;               // Silent mode?
-	BOSS_COMMON extern bool gl_show_CRCs;            // Whether or not to show mod CRCs.
-	BOSS_COMMON extern bool gl_trial_run;            // If true, don't redate files.
 
-	// Repository URLs
-	BOSS_COMMON extern std::string gl_oblivion_repo_url;
-	BOSS_COMMON extern std::string gl_nehrim_repo_url;
-	BOSS_COMMON extern std::string gl_skyrim_repo_url;
-	BOSS_COMMON extern std::string gl_fallout3_repo_url;
-	BOSS_COMMON extern std::string gl_falloutnv_repo_url;
+///////////////////////////////
+// Ini Settings
+///////////////////////////////
+// These globals exist for ease-of-use, so that a Settings object doesn't need to be passed in infinity+1 functions.
+
+// General variables
+BOSS_COMMON extern bool gl_use_user_rules_manager;   // Use the User Rules Editor or edit userlist.txt directly?
+BOSS_COMMON extern bool gl_close_gui_after_sorting;  // Close the GUI after BOSS has finished running or not.
+BOSS_COMMON extern uint32_t gl_language;             // What language to run BOSS in?
+
+// Command line variables
+BOSS_COMMON extern uint32_t gl_log_format;       // What format the output should be in.  Uses the enums defined above.
+BOSS_COMMON extern uint32_t gl_game;             // What game's mods are we sorting? Uses the enums defined above.
+BOSS_COMMON extern uint32_t gl_last_game;        // What game was last run? Only affects GUI behaviour.
+BOSS_COMMON extern uint32_t gl_revert;           // What level to revert to
+BOSS_COMMON extern uint32_t gl_debug_verbosity;  // Log levels above INFO to output
+BOSS_COMMON extern bool gl_update;               // Update the masterlist?
+BOSS_COMMON extern bool gl_update_only;          // Only update the masterlist and don't sort currently.
+BOSS_COMMON extern bool gl_silent;               // Silent mode?
+BOSS_COMMON extern bool gl_show_CRCs;            // Whether or not to show mod CRCs.
+BOSS_COMMON extern bool gl_trial_run;            // If true, don't redate files.
+
+// Repository URLs
+BOSS_COMMON extern std::string gl_oblivion_repo_url;
+BOSS_COMMON extern std::string gl_nehrim_repo_url;
+BOSS_COMMON extern std::string gl_skyrim_repo_url;
+BOSS_COMMON extern std::string gl_fallout3_repo_url;
+BOSS_COMMON extern std::string gl_falloutnv_repo_url;
+
 }  // namespace boss
 #endif
