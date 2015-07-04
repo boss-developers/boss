50c00293b66a285fc5e692a211b919a15d0e1c8c
deaths_soul (MCP) <NULL>
2015-07-04 10:13:19 -0400

    Deindenting the namespace in Common/Error.h

--- src/Common/Error.h
+++ src/Common/Error.h
@@ -38,121 +38,120 @@
 #include "Common/DllDef.h"
 
 namespace boss {
-	using namespace std;
 
-	namespace loc = boost::locale;
+using namespace std;
 
-	// MCP Note: Possibly change these to enums?
-	// Return codes, mostly error codes.
-	BOSS_COMMON extern const uint32_t BOSS_OK;
+namespace loc = boost::locale;
 
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_READ_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_WRITE_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_NOT_UTF8;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_NOT_FOUND;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_PARSE_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_CONDITION_EVAL_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_REGEX_EVAL_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_NO_GAME_DETECTED;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_ENCODING_CONVERSION_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_PLUGIN_BEFORE_MASTER;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_INVALID_SYNTAX;
+// MCP Note: Possibly change these to enums?
+// Return codes, mostly error codes.
+BOSS_COMMON extern const uint32_t BOSS_OK;
 
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_GIT_ERROR;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_READ_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_WRITE_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_NOT_UTF8;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_NOT_FOUND;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FILE_PARSE_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_CONDITION_EVAL_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_REGEX_EVAL_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_NO_GAME_DETECTED;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_ENCODING_CONVERSION_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_PLUGIN_BEFORE_MASTER;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_INVALID_SYNTAX;
 
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_MOD_TIME_READ_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_MOD_TIME_WRITE_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_RENAME_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_DELETE_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_CREATE_DIRECTORY_FAIL;
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_ITER_DIRECTORY_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_GIT_ERROR;
 
-	BOSS_COMMON extern const uint32_t BOSS_ERROR_GUI_WINDOW_INIT_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_MOD_TIME_READ_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_MOD_TIME_WRITE_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_RENAME_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_FILE_DELETE_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_CREATE_DIRECTORY_FAIL;
+BOSS_COMMON extern const uint32_t BOSS_ERROR_FS_ITER_DIRECTORY_FAIL;
 
-	class BOSS_COMMON boss_error {
-	public:
-		// For general errors not referencing specific files.
-		boss_error(const uint32_t internalErrCode);
+BOSS_COMMON extern const uint32_t BOSS_ERROR_GUI_WINDOW_INIT_FAIL;
 
-		// For general errors referencing specific files.
-		boss_error(const uint32_t internalErrCode,
-		           const string internalErrSubject);
+class BOSS_COMMON boss_error {
+public:
+	// For general errors not referencing specific files.
+	boss_error(const uint32_t internalErrCode);
 
-		// For errors from BOOST Filesystem functions.
-		boss_error(const uint32_t internalErrCode,
-		           const string internalErrSubject,
-		           const string externalErrString);
+	// For general errors referencing specific files.
+	boss_error(const uint32_t internalErrCode, const string internalErrSubject);
 
-		//For errors from other external functions.
-		boss_error(const string externalErrString,
-		           const uint32_t internalErrCode);
+	// For errors from BOOST Filesystem functions.
+	boss_error(const uint32_t internalErrCode, const string internalErrSubject,
+	           const string externalErrString);
 
-		// Returns the error code for the object.
-		uint32_t getCode() const;
+	//For errors from other external functions.
+	boss_error(const string externalErrString, const uint32_t internalErrCode);
 
-		// Returns the error string for the object.
-		string getString() const;
+	// Returns the error code for the object.
+	uint32_t getCode() const;
 
-	private:
-		uint32_t errCode;
-		string errString;
-		string errSubject;
-	};
+	// Returns the error string for the object.
+	string getString() const;
 
-	// Parsing error formats.
-	static boost::format MasterlistParsingErrorHeader(loc::translate("Masterlist Parsing Error: Expected a %1% at:"));
-	static boost::format IniParsingErrorHeader(loc::translate("Ini Parsing Error: Expected a %1% at:"));
-	static boost::format RuleListParsingErrorHeader(loc::translate("Userlist Parsing Error: Expected a %1% at:"));
-	static boost::format RuleListSyntaxErrorMessage(loc::translate("Userlist Syntax Error: The rule beginning \"%1%: %2%\" %3%"));
-	static const string MasterlistParsingErrorFooter(loc::translate("Masterlist parsing aborted. Utility will end now."));
-	static const string IniParsingErrorFooter(loc::translate("Ini parsing aborted. Some or all of the options may not have been set correctly."));
-	static const string RuleListParsingErrorFooter(loc::translate("Userlist parsing aborted. No rules will be applied."));
+private:
+	uint32_t errCode;
+	string errString;
+	string errSubject;
+};
 
-	// RuleList syntax error strings.
-	static const string ESortLineInForRule(loc::translate("includes a sort line in a rule with a FOR rule keyword."));
-	static const string EAddingModGroup(loc::translate("tries to add a group."));
-	static const string ESortingGroupEsms(loc::translate("tries to sort the group \"ESMs\"."));
-	static const string ESortingMasterEsm(loc::translate("tries to sort the master .ESM file."));
-	static const string EReferencingModAndGroup(loc::translate("references a mod and a group."));
-	static const string ESortingGroupBeforeEsms(loc::translate("tries to sort a group before the group \"ESMs\"."));
-	static const string ESortingModBeforeGameMaster(loc::translate("tries to sort a mod before the master .ESM file."));
-	static const string EInsertingToTopOfEsms(loc::translate("tries to insert a mod into the top of the group \"ESMs\", before the master .ESM file."));
-	static const string EInsertingGroupOrIntoMod(loc::translate("tries to insert a group or insert something into a mod."));
-	static const string EAttachingMessageToGroup(loc::translate("tries to attach a message to a group."));
-	static const string EMultipleSortLines(loc::translate("has more than one sort line."));
-	static const string EMultipleReplaceLines(loc::translate("has more than one REPLACE-using message line."));
-	static const string EReplaceNotFirst(loc::translate("has a REPLACE-using message line that is not the first message line."));
-	static const string ESortNotSecond(loc::translate("has a sort line that is not the second line of the rule."));
-	static const string ESortingToItself(loc::translate("tries to sort a mod or group relative to itself."));
-	static const string EAttachingNonMessage(loc::translate("tries to attach an malformatted message."));
-	static const string ESortingMasterAfterPlugin(loc::translate("tries to sort a plugin before a master file."));
-	static const string ESortingPluginBeforeMaster(loc::translate("tries to sort a master file before a plugin."));
+// Parsing error formats.
+static boost::format MasterlistParsingErrorHeader(loc::translate("Masterlist Parsing Error: Expected a %1% at:"));
+static boost::format IniParsingErrorHeader(loc::translate("Ini Parsing Error: Expected a %1% at:"));
+static boost::format RuleListParsingErrorHeader(loc::translate("Userlist Parsing Error: Expected a %1% at:"));
+static boost::format RuleListSyntaxErrorMessage(loc::translate("Userlist Syntax Error: The rule beginning \"%1%: %2%\" %3%"));
+static const string MasterlistParsingErrorFooter(loc::translate("Masterlist parsing aborted. Utility will end now."));
+static const string IniParsingErrorFooter(loc::translate("Ini parsing aborted. Some or all of the options may not have been set correctly."));
+static const string RuleListParsingErrorFooter(loc::translate("Userlist parsing aborted. No rules will be applied."));
 
-	// Parsing error class.
-	class BOSS_COMMON ParsingError {
-	public:
-		ParsingError();
+// RuleList syntax error strings.
+static const string ESortLineInForRule(loc::translate("includes a sort line in a rule with a FOR rule keyword."));
+static const string EAddingModGroup(loc::translate("tries to add a group."));
+static const string ESortingGroupEsms(loc::translate("tries to sort the group \"ESMs\"."));
+static const string ESortingMasterEsm(loc::translate("tries to sort the master .ESM file."));
+static const string EReferencingModAndGroup(loc::translate("references a mod and a group."));
+static const string ESortingGroupBeforeEsms(loc::translate("tries to sort a group before the group \"ESMs\"."));
+static const string ESortingModBeforeGameMaster(loc::translate("tries to sort a mod before the master .ESM file."));
+static const string EInsertingToTopOfEsms(loc::translate("tries to insert a mod into the top of the group \"ESMs\", before the master .ESM file."));
+static const string EInsertingGroupOrIntoMod(loc::translate("tries to insert a group or insert something into a mod."));
+static const string EAttachingMessageToGroup(loc::translate("tries to attach a message to a group."));
+static const string EMultipleSortLines(loc::translate("has more than one sort line."));
+static const string EMultipleReplaceLines(loc::translate("has more than one REPLACE-using message line."));
+static const string EReplaceNotFirst(loc::translate("has a REPLACE-using message line that is not the first message line."));
+static const string ESortNotSecond(loc::translate("has a sort line that is not the second line of the rule."));
+static const string ESortingToItself(loc::translate("tries to sort a mod or group relative to itself."));
+static const string EAttachingNonMessage(loc::translate("tries to attach an malformatted message."));
+static const string ESortingMasterAfterPlugin(loc::translate("tries to sort a plugin before a master file."));
+static const string ESortingPluginBeforeMaster(loc::translate("tries to sort a master file before a plugin."));
 
-		// For parsing errors.
-		ParsingError(const string inHeader, const string inDetail,
-		             const string inFooter);
+// Parsing error class.
+class BOSS_COMMON ParsingError {
+public:
+	ParsingError();
 
-		// For userlist syntax errors.
-		ParsingError(const string inWholeMessage);
+	// For parsing errors.
+	ParsingError(const string inHeader, const string inDetail,
+	             const string inFooter);
 
-		bool Empty() const;
-		string Header() const;
-		string Footer() const;
-		string Detail() const;
-		string WholeMessage() const;
+	// For userlist syntax errors.
+	ParsingError(const string inWholeMessage);
 
-		void Clear();
+	bool Empty() const;
+	string Header() const;
+	string Footer() const;
+	string Detail() const;
+	string WholeMessage() const;
 
-	private:
-		string header;
-		string footer;
-		string detail;
-		string wholeMessage;
-	};
+	void Clear();
+
+private:
+	string header;
+	string footer;
+	string detail;
+	string wholeMessage;
+};
+
 }  // namespace boss
 #endif
