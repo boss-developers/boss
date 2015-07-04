1e54dfbbc3f54bca1f6cc96c4c502329a25c20e3
deaths_soul (MCP) <NULL>
2015-07-04 11:38:40 -0400

    Deindenting the namespace in Support/VersionRegex.cpp

--- src/Support/VersionRegex.cpp
+++ src/Support/VersionRegex.cpp
@@ -31,50 +31,51 @@
 
 namespace boss {
 
-	using namespace std;
-	using namespace boost;
+using namespace std;
+using namespace boost;
 
-	/// REGEX expression definition
-	///  Each expression is composed of three parts:
-	///    1. The marker string "version", "ver", "rev", "v" or "r"
-	///    2. The version string itself.
+/// REGEX expression definition
+///  Each expression is composed of three parts:
+///    1. The marker string "version", "ver", "rev", "v" or "r"
+///    2. The version string itself.
 
-	const char* regex1 =
-	    "^(?:\\bversion\\b[ ]*(?:[:.\\-]?)|\\brevision\\b(?:[:.\\-]?))[ ]*"
-	    "((?:alpha|beta|test|debug)?\\s*[-0-9a-zA-Z._+]+\\s*(?:alpha|beta|test|debug)?\\s*(?:[0-9]*))$";
+const char* regex1 =
+    "^(?:\\bversion\\b[ ]*(?:[:.\\-]?)|\\brevision\\b(?:[:.\\-]?))[ ]*"
+    "((?:alpha|beta|test|debug)?\\s*[-0-9a-zA-Z._+]+\\s*(?:alpha|beta|test|debug)?\\s*(?:[0-9]*))$";
 
-	const char* regex2 =
-	    "(?:\\bversion\\b(?:[ :]?)|\\brevision\\b(?:[:.\\-]?))[ ]*"
-	    "([0-9][-0-9a-zA-Z._]+\\+?)";
+const char* regex2 =
+    "(?:\\bversion\\b(?:[ :]?)|\\brevision\\b(?:[:.\\-]?))[ ]*"
+    "([0-9][-0-9a-zA-Z._]+\\+?)";
 
-	const char* regex3 =
-	    "(?:\\bver(?:[:.]?)|\\brev(?:[:.]?))\\s*"
-	    "([0-9][-0-9a-zA-Z._]*\\+?)";
+const char* regex3 =
+    "(?:\\bver(?:[:.]?)|\\brev(?:[:.]?))\\s*"
+    "([0-9][-0-9a-zA-Z._]*\\+?)";
 
-	// Matches "Updated: <date>" for the Bashed patch
-	const char* regex4 =
-	    "(?:Updated:)\\s*"
-	    "([-0-9aAmMpP/ :]+)$";
+// Matches "Updated: <date>" for the Bashed patch
+const char* regex4 =
+    "(?:Updated:)\\s*"
+    "([-0-9aAmMpP/ :]+)$";
 
-	// Matches isolated versions as last resort
-	const char* regex5 =
-	    "(?:(?:\\bv|\\br)(?:\\s?)(?:[-.:])?(?:\\s*))"
-	    "((?:(?:\\balpha\\b)?|(?:\\bbeta\\b)?)\\s*[0-9][-0-9a-zA-Z._]*\\+?)";
+// Matches isolated versions as last resort
+const char* regex5 =
+    "(?:(?:\\bv|\\br)(?:\\s?)(?:[-.:])?(?:\\s*))"
+    "((?:(?:\\balpha\\b)?|(?:\\bbeta\\b)?)\\s*[0-9][-0-9a-zA-Z._]*\\+?)";
 
-	// Matches isolated versions as last resort
-	const char* regex6 =
-	    "((?:(?:\\balpha\\b)?|(?:\\bbeta\\b)?)\\s*\\b[0-9][-0-9a-zA-Z._]*\\+?)$";
+// Matches isolated versions as last resort
+const char* regex6 =
+    "((?:(?:\\balpha\\b)?|(?:\\bbeta\\b)?)\\s*\\b[0-9][-0-9a-zA-Z._]*\\+?)$";
 
-	const char* regex7 =
-	    "(^\\bmark\\b\\s*\\b[IVX0-9][-0-9a-zA-Z._+]*\\s*(?:alpha|beta|test|debug)?\\s*(?:[0-9]*)?)$";
+const char* regex7 =
+    "(^\\bmark\\b\\s*\\b[IVX0-9][-0-9a-zA-Z._+]*\\s*(?:alpha|beta|test|debug)?\\s*(?:[0-9]*)?)$";
 
-	/// Array used to try each of the expressions defined above using
-	/// an iteration for each of them.
-	regex* version_checks[] = {new regex(regex1, regex::icase),
-	                           new regex(regex2, regex::icase),
-	                           new regex(regex3, regex::icase),
-	                           new regex(regex4, regex::icase),
-	                           new regex(regex5, regex::icase),  // This incorrectly identifies "OBSE v19" where 19 is any integer.
-	                           new regex(regex6, regex::icase),  // This is responsible for metallicow's false positive.
-	                           new regex(regex7, regex::icase), 0};
+/// Array used to try each of the expressions defined above using
+/// an iteration for each of them.
+regex* version_checks[] = {new regex(regex1, regex::icase),
+                           new regex(regex2, regex::icase),
+                           new regex(regex3, regex::icase),
+                           new regex(regex4, regex::icase),
+                           new regex(regex5, regex::icase),  // This incorrectly identifies "OBSE v19" where 19 is any integer.
+                           new regex(regex6, regex::icase),  // This is responsible for metallicow's false positive.
+                           new regex(regex7, regex::icase), 0};
+
 }  // namespace boss
